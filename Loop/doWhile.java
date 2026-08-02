192dc68e1149ecc193eb21b9529ea021353e5b54
20pari57 <pariaarijita52@gmail.com>
2026-08-02 23:28:03 +0530

    loop done

---
+++ Loop/doWhile.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class doWhile {
+public static void main(String args []) {
+     Scanner sc = new Scanner (System.in);
+        int i=90;
+        do {
+            
+            System.out.println("The number is not 90");
+        }
+        while(i!=90);
+    }
+}    
